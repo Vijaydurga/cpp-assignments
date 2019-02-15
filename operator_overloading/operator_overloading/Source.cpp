794fa68402254ee2799a430e3fad7157178f9dcf
cdac <cdac@DESKTOP-SS5H7HT>
2019-02-15 15:24:09 -0800

    cpp-assignments

---
+++ operator_overloading/operator_overloading/Source.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<cstdlib>
+using namespace std;
+class complex
+{
+	int real, img;
+public:
+	void data()
+	{
+		cin >> real >> img;
+	}
+	friend ostream& operator<<(ostream& cout, complex);
+};
+ostream& operator<<(ostream& cout, complex c)
+{
+	cout << "real=" << c.real << endl;
+	cout << "img=" << c.img << endl;
+	return(cout);
+}
+int main()
+{
+	complex c3;
+	c3.data();
+	cout << c3;
+	system("pause");
+}
